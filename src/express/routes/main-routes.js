64c8b9dbe6552661238dce10c1595d98db98eb19
julia-marta <marta237@yandex.ru>
2021-02-11 04:03:38 +0300

    Устанавливает pug, добавляет шаблоны для всех страниц, согласно ТЗ, корректирует обработчики маршрутов, запускает сервер и проверяет корректность работы приложения

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,10 +3,10 @@
 const {Router} = require(`express`);
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.send(`/`));
-mainRouter.get(`/register`, (req, res) => res.send(`/register`));
-mainRouter.get(`/login`, (req, res) => res.send(`/login`));
-mainRouter.get(`/search`, (req, res) => res.send(`/search`));
-mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
+mainRouter.get(`/`, (req, res) => res.render(`main`, {user: {type: `guest`}}));
+mainRouter.get(`/register`, (req, res) => res.render(`sign-up`, {user: {type: `guest`}}));
+mainRouter.get(`/login`, (req, res) => res.render(`login`, {user: {type: `guest`}}));
+mainRouter.get(`/search`, (req, res) => res.render(`search`, {user: {type: `guest`}, wrapper: `wrapper-color`}));
+mainRouter.get(`/categories`, (req, res) => res.render(`all-categories`, {user: {type: `admin`}, wrapper: `wrapper wrapper--nobackground`}));
 
 module.exports = mainRouter;
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,7 +3,7 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(`/my`));
-myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+myRouter.get(`/`, (req, res) => res.render(`my/my`, {user: {type: `admin`}, wrapper: `wrapper wrapper--nobackground`}));
+myRouter.get(`/comments`, (req, res) => res.render(`my/comments`, {user: {type: `admin`}, wrapper: `wrapper wrapper--nobackground`}));
 
 module.exports = myRouter;
